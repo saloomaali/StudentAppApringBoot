0cf31069bd4992b426cfae0106458be52459583e
Salooma <saloomaali2000@gmail.com>
2022-12-08 15:17:10 +0530

    api created

--- src/main/java/com/example/studemtAppNew_Backend/controller/StudentController.java
+++ src/main/java/com/example/studemtAppNew_Backend/controller/StudentController.java
@@ -1,23 +1,34 @@
 package com.example.studemtAppNew_Backend.controller;
 
 import com.example.studemtAppNew_Backend.model.Students;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 
 public class StudentController {
 
-    @GetMapping("/")
-    public String Homepage(){
-        return "welcome to my website";
-    }
-
+    @CrossOrigin(origins = "*")
     @PostMapping(path = "/add", consumes = "application/json", produces = "application/json")
     public String AddStudent(@RequestBody Students s){
         System.out.println(s.getName().toString());
         return "student added successfully";
     }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping(path = "/view")
+    public String viewStudent(@RequestBody Students s){
+        return "";
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/search")
+    public String searchStudent(){
+        return "";
+    }
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/delete")
+    public String deleteStudent(){
+        return "";
+    }
+
 }
